5ebd07a18a63c8af44a87fbfaafd1af3a50bb780
Patrick <pds2k12@live.com>
2020-12-20 12:33:18 +0000

    atm improvements, added player body shots data

---
+++ scriptfiles/player_shots.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE player_shots_stats (
+    uid int(11) NOT NULL,
+    chest int(11) NOT NULL DEFAULT 0,
+    torso int(11) NOT NULL DEFAULT 0,
+    left_arm int(11) NOT NULL DEFAULT 0,
+    right_arm int(11) NOT NULL DEFAULT 0,
+    left_leg int(11) NOT NULL DEFAULT 0,
+    right_leg int(11) NOT NULL DEFAULT 0,
+    head int(11) NOT NULL DEFAULT 0,
+    misses int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY
+        (uid),
+    FOREIGN KEY
+        (uid)
+    REFERENCES
+        players (u_id)
+    ON
+        DELETE CASCADE
+    ON
+        UPDATE CASCADE
+);
