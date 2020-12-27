82a253886add24a7c7e38171596a2ebf984963c1
Patrick <pds2k12@live.com>
2020-12-27 15:37:47 +0000

    fix player_shots playerid -> issuerid. add saving to player_items + /giveitem command.

---
+++ scriptfiles/player_items.sql
@@ -0,0 +1,21 @@
+CREATE TABLE player_items (
+    u_id int(11) NOT NULL,
+    c4 tinyint(4) NOT NULL DEFAULT 0,
+    crack tinyint(4) NOT NULL DEFAULT 0,
+    weed tinyint(4) NOT NULL DEFAULT 0,
+    picklock tinyint(4) NOT NULL DEFAULT 0,
+    wallet tinyint(4) NOT NULL DEFAULT 0,
+    rope tinyint(4) NOT NULL DEFAULT 0,
+    condom tinyint(4) NOT NULL DEFAULT 0,
+    scissors tinyint(4) NOT NULL DEFAULT 0,
+    UNIQUE KEY
+        u_id (u_id),
+    FOREIGN KEY
+        (u_id)
+    REFERENCES
+        players (u_id)
+    ON DELETE
+        CASCADE
+    ON UPDATE
+        CASCADE
+);
